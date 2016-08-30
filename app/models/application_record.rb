dbce09e0422a66d1b27016bb82745cc81f7f343f
hyanying <yan.and.ying@gmail.com>
2016-08-30 07:24:55 +0100

    cleaned up code

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,12 +1,3 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 end
-
-class Location < ActiveRecord::Base
-	acts_as_mappable :default_units => :kms,
-					 :default_formula => :sphere,
-					 :distance_field_name => :distance,
-					 :lat_column_name => :lat,
-					 :lng_column_name => :lng
-
-end
